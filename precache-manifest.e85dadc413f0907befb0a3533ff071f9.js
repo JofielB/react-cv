bd0684720ecc8a26649e0b8f867d718a99e449de
Jofiel Batista <jofielbatista@hotmail.com>
2020-10-10 15:38:08 -0600

    Updates

---
+++ precache-manifest.e85dadc413f0907befb0a3533ff071f9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2271c9a8a73db43ea6e2cc5fc9ec50c6",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "6b4d12df69c4541c64d1",
+    "url": "/react-cv/static/css/main.688bf585.chunk.css"
+  },
+  {
+    "revision": "77133fd8f0e3d63756f9",
+    "url": "/react-cv/static/js/2.0a6fd6d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-cv/static/js/2.0a6fd6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4d12df69c4541c64d1",
+    "url": "/react-cv/static/js/main.181a51b7.chunk.js"
+  },
+  {
+    "revision": "7ace52926e56a7c08c6e",
+    "url": "/react-cv/static/js/runtime-main.3005bd5b.js"
+  },
+  {
+    "revision": "0040a80ea88dd788667a6e55052b392d",
+    "url": "/react-cv/static/media/email.0040a80e.png"
+  },
+  {
+    "revision": "8df07bef3c647e9907f3b6ca2680b566",
+    "url": "/react-cv/static/media/github.8df07bef.png"
+  },
+  {
+    "revision": "e12cf0523cc7a646dab35c8993381a4f",
+    "url": "/react-cv/static/media/linkedin.e12cf052.png"
+  },
+  {
+    "revision": "3e15df147b94eb698381ad96b7db3d69",
+    "url": "/react-cv/static/media/phone.3e15df14.png"
+  }
+]);
